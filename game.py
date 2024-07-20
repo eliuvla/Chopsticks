cae59802414e632c44c4a8ea501c2557040cb8bb
Eliú <75458326+eliuvla@users.noreply.github.com>
2024-07-20 04:10:37 +0000

    cambios en game

--- game.py
+++ game.py
@@ -1,18 +1,18 @@
 import numpy as np
 from player import *
 
-class Position:
+class Game:
 
   def __init__(self, player_1, player_2):
     self.player_1 = player_1
     self.player_2 = player_2
-    self.turn = np.random.randint(0,2)
+    self.turn = np.random.randint(1,3)
     self.winner = None
 
   def check_win(self):
     if self.player_1.hand_1 + self.player_1.hand_2 == 0:
-      self.winner = 0
-      print('Player 2 wins!')
-    elif self.player_2.hand_1 + self.player_2.hand_2 == 0:
       self.winner = 1
       print('Player 1 wins!')
+    elif self.player_2.hand_1 + self.player_2.hand_2 == 0:
+      self.winner = 2
+      print('Player 2 wins!')
