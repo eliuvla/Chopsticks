14cb8cee2499cb768514d3b7e9de2bd2e83bf542
Eliú <75458326+eliuvla@users.noreply.github.com>
2024-07-20 04:53:12 +0000

    print function

--- game.py
+++ game.py
@@ -17,4 +17,8 @@ class Game:
       self.winner = 2
       print('Player 2 wins!')
 
-  def 
+  def print(self):
+      print("turn of player"+str(self.turn))
+      print("hands of player 1: "+str(self.player_1.hand_1)+" "+str(self.player_1.hand_2))
+      print("hands of player 2: "+str(self.player_2.hand_1)+" "+str(self.player_2.hand_2))
+
