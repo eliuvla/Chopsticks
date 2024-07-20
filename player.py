3b00d03715af920995c0cd336300572c00754025
Gabo-always-learning <159660211+Gabo-always-learning@users.noreply.github.com>
2024-07-19 21:03:37 -0700

    Primera versión de clase de jugador

--- player.py
+++ player.py
@@ -1,12 +1,16 @@
-
-
-
-def HelloWorld():
-    print("hello world")
-
-
-
 class Player:
-    def __init__(self):
-        self.hand_1 = 1
-        self.hand_2 = 1
+
+  def __init__(self, hand_1, hand_2):
+    self.hand_1 = hand_1
+    self.hand_2 = hand_2
+
+  def attack(self,hand_1, other,hand_2):
+    other.hand_2 = (other.hand_2 + self.hand_1) % 5
+    
+
+  def split(self, hand_1, hand_2):
+    if (hand_1 == 0 or hand_2 == 0) and (hand_1 + hand_2) % 2 == 0:
+      self.hand_1 = (self.hand_1+self.hand_2)/2
+      self.hand_2 = (self.hand_1+self.hand_2)/2
+    else:
+      print("No se puede hacer split")
