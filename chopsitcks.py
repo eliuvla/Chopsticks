b01a9c207c58804ef11e596e1a471c19485af840
gabriel-mirandag@outlook.com <gabriel-mirandag@outlook.com>
2024-07-20 18:30:16 -0700

    Primera version del juego terminada, pero hago commit con fines de correr el programa

--- chopsitcks.py
+++ chopsitcks.py
@@ -6,7 +6,16 @@ from player import *
 player_1 = Player(1,1)
 player_2 = Player(1,1)
 
-
 game = Game(player_1,player_2)
 
-game.print()
+while True:
+    game.turn
+    if game.turn == 1:
+        game.action(player_1,player_2)
+        if game.winner != None:
+            break
+    if game.turn == 2:
+        game.action(player_2,player_1)
+        if game.winner != None:
+            break
+
--- game.py
+++ game.py
@@ -22,3 +22,51 @@ class Game:
       print("hands of player 1: "+str(self.player_1.hand_1)+" "+str(self.player_1.hand_2))
       print("hands of player 2: "+str(self.player_2.hand_1)+" "+str(self.player_2.hand_2))
 
+  def turn(self,other):
+      self.turn = (self.turn + 1)%2 + 1
+      print(f'Player {self.turn} turn')
+
+  def action(player_1,player_2):   
+      if player_1.hand_1 != 0 and player_1.hand_2 != 0:
+        print('You can only attack')
+        while True:
+          print('Choose one of your hands (1 or 2)')
+          hand = input()
+          if hand == 1 or hand == 2:
+            break
+        while True:
+          print("Choose one of your opponent's hand (1 or 2)")         
+          hand_o = input
+          if hand_o == 1 or hand_o == 2:
+            break
+        player_1.attack(hand, player_2, hand_o)
+      elif (player_1.hand_1 == 0 or player_1.hand_2 == 0) and (player_1.hand_1 + player_2.hand_2)%2 == 0:
+        while True:
+          print('Choose to 1:attack or 2:spit')
+          action = input()
+          if action == 1 or action == 2:
+            break
+        if action == 1:
+          while True:
+            print('Choose one of your hands (1 or 2)')
+            hand = input()
+            if hand == 1 or hand == 2:
+              break
+          while True:
+            print("Choose one of your opponent's hand (1 or 2)")         
+            hand_o = input
+            if hand_o == 1 or hand_o == 2:
+              break
+          player_1.attack(hand, player_2, hand_o)
+        else:
+          if player_1.hand_1 == 0:
+            player_1.hand_1 = player_1.hand_1/2
+            player_1.hand_2 = player_1.hand_1/2
+          elif player_1.hand_2 == 0:
+            player_1.hand_1 = player_1.hand_2/2
+            player_1.hand_2 = player_1.hand_2/2
+
+
+        
+        
+
